75357903c5a0651f8447812391d5ac136b7297de
IuriVeloso <iuriv.felix@gmail.com>
2024-09-05 10:21:45 -0300

    feat: edit county ids

--- src/assets/maps/index.js
+++ src/assets/maps/index.js
@@ -9,16 +9,39 @@ import zipRioDeJaneiroUrl from "./county/rio_de_janeiro.zip";
 import zipSalvadorUrl from "./county/salvador.zip";
 import zipSaoPauloUrl from "./county/sao_paulo.zip";
 
+// import zipProdBelemUrl from "/build/static/media/belem.b0e2a99a09d0d2a32977.zip";
+// import zipProdBeloHorizonteUrl from "/build/static/media/belo_horizonte.f1c971592e40880f2455.zip";
+// import zipProdCuritibaUrl from "/build/static/media/curitiba.6a012270e1d979c0afea.zip";
+// import zipProdFortalezaUrl from "/build/static/media/fortaleza.573d0dc789b79a3b6e5b.zip";
+// import zipProdManausUrl from "/build/static/media/manaus.87ca7e85881255a55656.zip";
+// import zipProdPortoAlegreUrl from "/build/static/media/porto_alegre.dbdd9b40aa3d02477151.zip";
+// import zipProdRecifeUrl from "/build/static/media/recife.074ba9ca36e91e0dacdd.zip";
+// import zipProdRioDeJaneiroUrl from "/build/static/media/rio_de_janeiro.961460a3a831233c4d78.zip";
+// import zipProdSalvadorUrl from "/build/static/media/salvador.4a67077d2ffbeb33fc92.zip";
+// import zipProdSaoPauloUrl from "/build/static/media/sao_paulo.cb7acf26706c69d6538b.zip";
+
+// export const zipCountyUrl = {
+//   210: process.env.NODE_ENV === 'production' ? zipProdBelemUrl : zipBelemUrl,
+//   27: process.env.NODE_ENV === 'production' ? zipProdBeloHorizonteUrl : zipBeloHorizonteUrl,
+//   194: process.env.NODE_ENV === 'production' ? zipProdCuritibaUrl : zipCuritibaUrl,
+//   1963: process.env.NODE_ENV === 'production' ? zipProdFortalezaUrl : zipFortalezaUrl,
+//   62: process.env.NODE_ENV === 'production' ? zipProdManausUrl : zipManausUrl,
+//   59: process.env.NODE_ENV === 'production' ? zipProdPortoAlegreUrl : zipPortoAlegreUrl,
+//   117: process.env.NODE_ENV === 'production' ? zipProdRecifeUrl : zipRecifeUrl,
+//   31: process.env.NODE_ENV === 'production' ? zipProdRioDeJaneiroUrl : zipRioDeJaneiroUrl,
+//   116: process.env.NODE_ENV === 'production' ? zipProdSalvadorUrl : zipSalvadorUrl,
+//   37: process.env.NODE_ENV === 'production' ? zipProdSaoPauloUrl : zipSaoPauloUrl,
+// };
 
 export const zipCountyUrl = {
-  210: zipBelemUrl,
-  27: zipBeloHorizonteUrl,
-  194: zipCuritibaUrl,
-  1963: zipFortalezaUrl,
-  62: zipManausUrl,
-  59: zipPortoAlegreUrl,
-  117: zipRecifeUrl,
-  31: zipRioDeJaneiroUrl,
-  116: zipSalvadorUrl,
-  37: zipSaoPauloUrl,
+  10: zipBelemUrl,
+  1: zipBeloHorizonteUrl,
+  9: zipCuritibaUrl,
+  4: zipFortalezaUrl,
+  6: zipManausUrl,
+  5: zipPortoAlegreUrl,
+  8: zipRecifeUrl,
+  2: zipRioDeJaneiroUrl,
+  7: zipSalvadorUrl,
+  3: zipSaoPauloUrl,
 };
