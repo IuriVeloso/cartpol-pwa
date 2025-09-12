76f8d9aa98d16c92057317e028d0b0c2105a91f3
Diana Monteiro <dianar.monteiro@gmail.com>
2025-09-12 15:42:58 -0300

    feat: add changes to home and navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import HomeLogo from "../../assets/logos/lappcom_logo.png";
 
-// import { Container } from './styles';
-
+import NavButton from "components/Buttons/NavButton";
 import "./styles.css";
 
 const Header: React.FC = () => {
   return (
     <div className="header-container">
-      <img src={HomeLogo} alt="Lappcom Logo"/>
-      <h1 className="header-title">CartPol</h1>
-      <a href="/" className="header-link">Como Usar</a>
-      <a href="/tool" className="header-link">Acessar Ferramenta</a>
+      <img src={HomeLogo} alt="Lappcom Logo" />
+      <div>
+        <NavButton className="header-title" href="/" text="CartPol" />
+        <NavButton href="/how-to-use" text="Como Usar" />
+        <NavButton href="/tool" text="Acessar Ferramenta" />
+      </div>
     </div>
   );
 };
